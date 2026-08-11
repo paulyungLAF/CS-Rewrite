15d69756d096de0d4909abd6edc7428fa1312ca5
Paul Yung <1913312@fitnessintl.com>
2026-08-11 12:55:26 -0700

    latest

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [MatTabsModule, RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
